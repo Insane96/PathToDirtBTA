cd98af0e356090fcc0fd961c7b832b01c304eaaf
Alberto Del Villano <delvillano.alberto@gmail.com>
2024-12-27 21:34:02 +0100

    Removed un-needed stuff

--- src/main/java/insane96mcp/pathtodirt/PathToDirt.java
+++ src/main/java/insane96mcp/pathtodirt/PathToDirt.java
@@ -1,9 +1,6 @@
 package insane96mcp.pathtodirt;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.core.data.tag.Tag;
-import net.minecraft.core.item.Item;
-import net.minecraft.core.item.tag.ItemTags;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.GameStartEntrypoint;
@@ -11,20 +8,11 @@ import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
 public class PathToDirt implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
-	public static Tag<Item> SHOVELS = Tag.of("shovels");
-
     public static final String MOD_ID = "pathtodirt";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
         LOGGER.info("PathToDirt initialized.");
-		ItemTags.TAG_LIST.add(SHOVELS);
-		SHOVELS.tag(Item.toolShovelWood);
-		SHOVELS.tag(Item.toolShovelStone);
-		SHOVELS.tag(Item.toolShovelIron);
-		SHOVELS.tag(Item.toolShovelGold);
-		SHOVELS.tag(Item.toolShovelDiamond);
-		SHOVELS.tag(Item.toolShovelSteel);
     }
 
 	@Override
